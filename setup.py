bfcf7db26e5148c1e3e7553cdf8c78c0ca4c9e4d
Federico Galatolo <galatolo.federico@gmail.com>
2021-05-22 12:14:54 +0200

    bump to 0.1.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "README.md")
 
 setuptools.setup(
     name="manim_presentation",
-    version="0.1.4",
+    version="0.1.5",
     author="Federico A. Galatolo",
     author_email="federico.galatolo@ing.unipi.it",
     description="Tool for live presentations using manim",
