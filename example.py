b9eb2b395abb226290f349b875004a0f7b390643
Jérome Eertmans <jeertmans@icloud.com>
2022-07-12 17:11:48 +0200

    (WIP): Add 3D Scene example

--- example.py
+++ example.py
@@ -1,5 +1,7 @@
 from manim import *
-from manim_presentation import Slide
+
+from manim_slides import Slide, ThreeDSlide
+
 
 class Example(Slide):
     def construct(self):
@@ -16,13 +18,35 @@ class Example(Slide):
         self.play(dot.animate.move_to(ORIGIN))
         self.pause()
 
-        self.play(dot.animate.move_to(RIGHT*3))
+        self.play(dot.animate.move_to(RIGHT * 3))
         self.pause()
-        
+
         self.start_loop()
         self.play(MoveAlongPath(dot, circle), run_time=2, rate_func=linear)
         self.end_loop()
 
         # Each slide MUST end with an animation (a self.wait is considered an animation)
         self.play(dot.animate.move_to(ORIGIN))
-        
+
+class ThreeDExample(ThreeDSlide):
+    def construct(self):
+        axes = ThreeDAxes()
+        circle=Circle()
+
+        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
+        self.add(circle,axes)
+
+        self.begin_ambient_camera_rotation(rate=0.1)
+        self.pause()
+
+        self.stop_ambient_camera_rotation()
+        self.move_camera(phi=75 * DEGREES, theta=30 * DEGREES)
+        self.wait()
+
+        self.start_loop()
+        self.play(circle.animate.move_to(ORIGIN + UP))
+        self.end_loop()
+
+        self.play(circle.animate.move_to(ORIGIN))
+
+        # TODO: fixit
