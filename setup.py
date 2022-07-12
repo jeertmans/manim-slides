ddeb20646d546bf1b2af93620be0d2baf54a0181
Jérome Eertmans <jeertmans@icloud.com>
2022-07-12 17:10:54 +0200

    Update setup file with personal info & deps

--- setup.py
+++ setup.py
@@ -1,32 +1,45 @@
-import setuptools
-import os
+import sys
 
-with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "README.md"), "r") as fh:
-    long_description = fh.read()
+import setuptools
+
+from manim_slides import __version__ as version
+
+if sys.version_info < (3, 7):
+    raise RuntimeError("This package requires Python 3.7+")
+
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setuptools.setup(
-    name="manim_presentation",
-    version="0.2.1",
-    author="Federico A. Galatolo",
-    author_email="federico.galatolo@ing.unipi.it",
+    name="manim-slides",
+    version=version,
+    author="Jérome Eertmans (previously, Federico A. Galatolo)",
+    author_email="jeertmans@icloud.com (resp., federico.galatolo@ing.unipi.it)",
     description="Tool for live presentations using manim",
-    url="https://github.com/galatolofederico/manim-presentation",
+    url="https://github.com/jeertmans/manim-slides",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    entry_points = {
+    entry_points={
         "console_scripts": [
-            "manim_presentation=manim_presentation.present:main",
-            "manim-presentation=manim_presentation.present:main",
-            "manim-presentation-wizard=manim_presentation.wizard:main",
+            "manim-slides=manim_slides.main:cli",
         ],
     },
+    python_requires=">=3.7",
     install_requires=[
+        "click>=8.0",
+        "click-default-group>=1.2"
+        "numpy>=1.19.3",
+        "pydantic>=1.9.1",
+        "opencv-python>=4.6",
     ],
     classifiers=[
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
-        "Development Status :: 4 - Beta"
     ],
 )
