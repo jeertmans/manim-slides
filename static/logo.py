c6c19bce893876be607fe52cf126d1b9f6a31ccc
Jérome Eertmans <jeertmans@icloud.com>
2022-09-14 18:52:39 +0200

    misc(logo): update logo

--- static/logo.py
+++ static/logo.py
@@ -4,7 +4,7 @@ from manim import *
 class ManimSlidesLogo(Scene):
     def construct(self):
         tex_template = TexTemplate()
-        tex_template.add_to_preamble(r"\usepackage{fontawesome5}")
+        tex_template.add_to_preamble(r"\usepackage{graphicx}\usepackage{fontawesome5}")
         self.camera.background_color = "#ffffff"
         logo_green = "#87c2a5"
         logo_blue = "#525893"
@@ -15,16 +15,25 @@ class ManimSlidesLogo(Scene):
         slides = MathTex(r"\mathbb{S}\text{lides}", fill_color=logo_black).scale(4)
         slides.next_to(ds_m, DOWN)
         slides.shift(DOWN)
-        book = Tex(
+        play = Tex(
             r"\faStepBackward\faStepForward",
             fill_color=logo_black,
             tex_template=tex_template,
         ).scale(4)
-        book.next_to(ds_m, LEFT)
-        book.shift(LEFT + 0.5 * DOWN)
+        play.next_to(ds_m, LEFT)
+        play.shift(LEFT + 0.5 * DOWN)
+        comment = Tex(
+            r"\reflectbox{\faComment*[regular]}",
+            fill_color=logo_black,
+            tex_template=tex_template,
+        ).scale(9)
+        comment.move_to(play)
+        comment.shift(0.4 * DOWN)
         circle = Circle(color=logo_green, fill_opacity=1).shift(LEFT)
         square = Square(color=logo_blue, fill_opacity=1).shift(UP)
         triangle = Triangle(color=logo_red, fill_opacity=1).shift(RIGHT)
-        logo = VGroup(triangle, square, circle, ds_m, slides, book)  # order matters
+        logo = VGroup(
+            triangle, square, circle, ds_m, slides, comment, play
+        )  # order matters
         logo.move_to(ORIGIN)
         self.add(logo)
