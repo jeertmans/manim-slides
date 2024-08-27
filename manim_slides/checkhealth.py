f11226d90f4dcd7927cb249892dc7045d76130e8
Jérome Eertmans <jeertmans@icloud.com>
2024-08-27 11:58:48 +0200

    chore(tests): implement some basic tests

--- manim_slides/checkhealth.py
+++ manim_slides/checkhealth.py
@@ -7,8 +7,33 @@ from .__version__ import __version__
 
 @click.command()
 def checkhealth() -> None:
-    """
-    Check Manim Slides' installation.
-    """
+    """Check Manim Slides' installation."""
     click.echo(f"Manim Slides version: {__version__}")
     click.echo(f"Python executable: {sys.executable}")
+    click.echo("Manim bindings:")
+
+    click.echo(f"Modules: {sys.modules.keys()}")
+
+    try:
+        from manim import __version__ as manimce_version
+
+        click.echo(f"\tmanim (version: {manimce_version})")
+    except ImportError:
+        click.secho("\tmanim not found", bold=True)
+
+    try:
+        from manimlib import __version__ as manimlib_version
+
+        click.echo(f"\tmanimgl (version: {manimlib_version})")
+    except ImportError:
+        click.secho("\tmanimgl not found", bold=True)
+
+    try:
+        from qtpy import API, QT_VERSION
+
+        click.echo(f"Qt API: {API} (version: {QT_VERSION})")
+    except ImportError:
+        click.secho(
+            "No Qt API found, some Manim Slides commands will not be available",
+            bold=True,
+        )
