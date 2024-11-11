c25bd5b94c30b80406ee46d1a0a019050a949877
PeculiarProgrammer <179261820+PeculiarProgrammer@users.noreply.github.com>
2024-11-11 11:12:58 -0500

    Fix workflow bug
    pre-commit.ci informed me that line 51 of manim_slides/convert.py shouldn't have the no-type checker on my computer, but it is required on the workflow.

--- manim_slides/convert.py
+++ manim_slides/convert.py
@@ -48,7 +48,7 @@ def open_with_default(file: Path) -> None:
     if system == "Darwin":
         subprocess.call(("open", str(file)))
     elif system == "Windows":
-        os.startfile(str(file))
+        os.startfile(str(file))  # type: ignore[attr-defined]
     else:
         subprocess.call(("xdg-open", str(file)))
 
