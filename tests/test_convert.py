e50271b0b2bc6623ce41ebbe13058d513de6af97
Jérome Eertmans <jeertmans@icloud.com>
2025-01-03 09:19:41 +0100

    feat(cli): document config options (#485)
    * feat(cli): document config options
    
    * chpre(docs): add changelog entry
    
    * fixes
    
    * chore(deps): remove restriction
    
    * fix(deps): PySide6 issue
    
    See https://github.com/astral-sh/uv/issues/10056
    
    * chore(docs): document config options
    
    * chore(fmt): auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fox(docs): deps
    
    * chore(tests): remove old test
    
    * chore(fmt): auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -3,7 +3,6 @@ from enum import EnumMeta
 from pathlib import Path
 
 import pytest
-from pydantic import ValidationError
 
 from manim_slides.config import PresentationConfig
 from manim_slides.convert import (
@@ -225,10 +224,6 @@ class TestConverter:
             presentation_configs=[presentation_config], frame_index=frame_index
         ).convert_to(out_file)
         assert out_file.exists()
-
-    def test_converter_no_presentation_config(self) -> None:
-        with pytest.raises(ValidationError):
-            Converter(presentation_configs=[])
 
     def test_pptx_converter(
         self, tmp_path: Path, presentation_config: PresentationConfig
