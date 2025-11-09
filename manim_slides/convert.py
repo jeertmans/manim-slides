38cd73bd39f7667b1fd517140868c7f8f08686fb
Christopher Albert <albert@tugraz.at>
2025-11-09 20:19:09 +0100

    fix: add final frame after subsections in PDF all mode

--- manim_slides/convert.py
+++ manim_slides/convert.py
@@ -788,10 +788,13 @@ class PDF(Converter):
 
     def _images_for_slide(self, slide_config: SlideConfig) -> list[Image]:
         if self.subsection_mode == SubsectionMode.all and slide_config.subsections:
-            return [
+            frames = [
                 self._frame_from_subsection(slide_config, subsection)
                 for subsection in slide_config.subsections
             ]
+            # Add final frame if there's content after last subsection
+            frames.append(self._frame_for_slide(slide_config))
+            return frames
 
         return [self._frame_for_slide(slide_config)]
 
